b4556fe60ec393237d32237887b1f0b99a847e8a
yusronalkayiss <cimasuk764@gmail.com>
2022-10-30 00:56:05 +0700

    Done

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+let weather = {
+  apiKey: '53166b8165f3541b1f1b9f8e036d0eb2',
+  fetchWeather: function (city) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + this.apiKey)
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector('.place-title').innerText = 'Weather in ' + name;
+    document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon + '.png';
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temprature').innerText = temp + '°C';
+    document.querySelector('.humidity').innerText = 'Humidity: ' + humidity + '%';
+    document.querySelector('.wind').innerText = 'Wind speed: ' + speed + ' km/h';
+    document.querySelector('.weather').classList.remove('loading');
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  searching: function () {
+    this.fetchWeather(document.querySelector('.search').value);
+  },
+};
+document.querySelector('.searching button').addEventListener('click', function () {
+  weather.searching();
+});
+document.querySelector('.search').addEventListener('keyup', function (event) {
+  if (event.key == 'Enter') {
+    weather.searching();
+  }
+});
+
+weather.fetchWeather('London');
